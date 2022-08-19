6755ea7403471629238a3c3828d920df54ea5e27
israell <israelpina047@gmail.com>
2022-08-19 17:48:07 -0300

    Concluido

--- Livros_de_Anne/src/App.java
+++ Livros_de_Anne/src/App.java
@@ -1,22 +1,48 @@
+import java.util.Scanner;
+
 public class App {
     public class ProjetinhoLivraria {
 
 
         public static void main(String[] args) {
-            Livro obj1;
-            Livro obj2;
-            obj1 = new Livro();
-            obj2 = new Livro();
-        }
-        public class MoverLivros{
-            public static void main(String[] args) {
-                obj1.titulo = "Anne de Green Gables ";
-                obj1.numpags = "256";
-                obj1.ano = "1908";
-                obj1.preco = "45.50";
-
-                
+            Livro P = new Livro();
+       
+            P.settitulo("Anne de Green Gables");
+            P.setnumpags(250);
+            P.setano(1901);
+            P.setpreco(35);
+    
+            System.out.println("\nTitulo seu Livro: " + P.gettitulo() + "\nNumero de paginas: " + P.getnumpags() + "\nO Ano do lançamento: " + P.getano() + "\nO Preço desse livro: " + P.getpreco() + ".");
+            
+            Livro S = new Livro();
+            S.settitulo("Anne De Windy Poplays");
+            S.setnumpags(350);
+            S.setano(1936);
+            S.setpreco(45);
+            System.out.println("\nTitulo do seu Segundo Livro: " + S.gettitulo() + "\nNumero de paginas: " + S.getnumpags() + "\nO Ano do lançamento: " + S.getano() + "\nO Preço desse livro: " + S.getpreco() + ".");
+    
+            try (Scanner ler = new Scanner(System.in)){
+                System.out.println("\n Digite o nome do novo Livro ");
+                String nome = ler.nextLine();
+                System.out.println("\n Digite o novo numero de paginas do seu livro ");
+                int A1 = ler.nextInt();
+                P.settitulo(nome);
+                System.out.println("\n Digite o novo ano do seu livro ");
+                int A2 = ler.nextInt();
+                System.out.println("\n Digite o preço do seu livro");
+                double A3 = ler.nextDouble();
+    
+                S.settitulo(nome);
+                S.setnumpags(A1);
+                S.setano(A2);
+                S.setpreco(A3);
+    
+            System.out.println("\nTitulo: " + S.gettitulo() + "\n NumerosdePags: " + S.getnumpags()+ "\n O Ano: " + S.getano() + "\n O Preço: " + S.getpreco( ) + ".");
+            
             }
-        }
+        
+        
+        }  
+     }
     }
-}
+
--- Livros_de_Anne/src/Livro.java
+++ Livros_de_Anne/src/Livro.java
@@ -1,9 +1,41 @@
 
 public class Livro {
    
-    String titulo;
-    int numpags;
-    int ano;
-    double preco;
+    private String titulo;
+    private int numpags;
+    private int ano;
+    private double preco;
+
+
+public String gettitulo(){
+    return titulo;
+}
+  public int getano(){
+     return ano;
+}
+
+public int getnumpags(){
+    return numpags;
+}
+public double getpreco(){
+    return preco;
+}
+
+ public void settitulo( String titulo) {
+    this.titulo = titulo;
+
+ }
+ public void setnumpags (int numpags) {
+    this.numpags = numpags;
+
+}
+public void setano (int ano) {
+    this.ano = ano;
+
+ }
+ public void setpreco ( double preco) {
+    this.preco = preco;
+
+  }
 
 }
